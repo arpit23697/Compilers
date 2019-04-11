5aca9ae969aaf6c3d922f353f453ed2c7af45dbf
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-04-11 10:19:53 +0530

    Added test case

--- project/out.js
+++ project/out.js
@@ -1,20 +1,53 @@
-function Fibonacci ( n  ){
-	if ( n == 0 )
-	 return 0;
-	else	if ( n == 1 )
-	 return 1;
-	else	 return ( Fibonacci (n - 1 ) + Fibonacci (n - 2 ) );
+function partition ( arr  , low  , high  ){
+	var temp;
+	var pivot = arr[high];
+	var i = ( low - 1 );
+	var j = low;
+	while ( j <= high - 1 )	{
+		if (arr[j] <= pivot )
+		{
+			var temp;
+			i++ ;
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			}
+		j++ ;
+		}
+	temp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = temp;
+	 return ( i + 1 );
+	}
+function quickSort ( arr  , low  , high  ){
+	if (low < high )
+	{
+		var pi = partition (arr,low,high );
+		quickSort (arr,low,pi - 1 );
+		quickSort (arr,pi + 1,high );
+		}
+	}
+function printArray ( arr  , size  ){
+	var i = 0;
+	while ( i < size )	{
+		document.write( arr[i]," ");
+		i++ ;
+		}
+	document.write( "<br>");
 	}
 function main (  ){
-	var n , i = 0 , c;
-	n = 30;
-	document.write( "Fibonacci series<br>");
-	c = 1;
-	while ( c <= n )	{
-		document.write( Fibonacci (i )," <br>");
+	var arr = new Array(100);
+	var n = 100;
+	var i = 0;
+	while ( i < n )	{
+		arr[i] = 100 - i;
 		i++ ;
-		c++ ;
 		}
+	document.write( "Before sorting <br>");
+	printArray (arr,n );
+	quickSort (arr,0,n - 1 );
+	document.write( "Sorted array: <br>");
+	printArray (arr,n );
 	 return 0;
 	}
 var s = main();
